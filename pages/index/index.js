c2cb6473f091c52d88a0d1f31e5fc03881a83640
YangKaiLin <yangkailin@yintai.com>
2017-01-18 18:22:35 +0800

    list

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,81 +9,87 @@ Page({
     swiperCurrent: 0,
     lists: [
         {
-            "redirecturl": "http://dongfeng.guoxinad.com.cn/xiaokang/dfxk_zp/",
+            "redirecturl": "234",
             "mainimg": "upload1/20160629/1606290358393979.jpg",
-            "title": "东风小康招贤纳士",
+            "title": "我是猎手",
             "description": "",
-            "content": "多维度介绍高效人才招聘"
+            "content": "I'M HUNTER!"
         },
         {
-            "redirecturl": "http://www.insun-china.com/item/insun2016/index.php",
+            "redirecturl": "../consigner/index",
             "mainimg": "upload1/20160629/1606291134207266.jpg",
-            "title": "在一起更锋芒",
+            "title": "我是委托人",
             "description": "",
-            "content": ""
+            "content": "I'M CONSIGNER!"
         },
         {
-            "redirecturl": "http://peugeot.insun-china.com/new4080814/index.html",
+            "redirecturl": "../admin/index",
             "mainimg": "upload1/20160202/1602020451004977.jpg",
-            "title": "新一代东风标致408问鼎而至",
+            "title": "我是管理员",
             "description": "",
-            "content": "品创意交互呈现产品匠心"
+            "content": "I'M ADMINISTRATIR!"
         },
         {
-            "redirecturl": "http://www.insun-china.com/item/insun_meeting/",
+            "redirecturl": "../hunter/index",
             "mainimg": "upload1/20160202/1602020545059897.jpg",
-            "title": "心相映 盛华章",
+            "title": "任务栏",
             "description": "",
-            "content": "映盛2015年年会邀请函"
+            "content": "TASKBOARD"
         },
         {
-            "redirecturl": "http://phptest.guoxinad.com.cn/thehanshow/hanshow_shuimo/",
-            "mainimg": "upload1/20160202/1602020424595729.jpg",
-            "title": "汉秀中国风邀请函",
-            "description": "",
-            "content": "中国风设计感，古典元素大气之作"
-        },
-        {
-            "redirecturl": "http://dongfeng.guoxinad.com.cn/dfct_c3xr_manhua/",
+            "redirecturl": "../taskboard/index",
             "mainimg": "upload1/20160202/1602020413397202.jpg",
-            "title": "探索星际之外不一样的精彩",
+            "title": "更多敬请期待",
             "description": "",
-            "content": "以次电影情节为主线的漫画故事型H5"
+            "content": "TO BE CONTINUE..."
         }
     ]
   },
   swiperChange: function(e){
-      this.setData({
-          swiperCurrent: e.detail.current
-      })
-  },
-  bindhunterViewTap: function() {
+        this.setData({
+            swiperCurrent: e.detail.current
+        })
+    },
+  bindViewTap:function(event){
+    console.log(event.target)
     wx.navigateTo({
-      url: '../hunter/index'
+      url: event.target.dataset.url
+      // url: event.target.dataset.redirecturl
     })
   },
-  bindconsignerViewTap: function() {
+  bindListViewTap: function(event) {
+    console.log(event.target)
     wx.navigateTo({
-      url: '../consigner/index'
+      url: event.target.dataset.redirecturl
     })
+    console.log(event.target.dataset.redirectUrl)
   },
-  bindadminViewTap: function() {
-    wx.navigateTo({
-      url: '../admin/index'
-    })
-  },
-  bindtaskboardViewTap: function() {
-    wx.navigateTo({
-       url: '../taskboard/index'
-    })
-  },
-  bindmoreViewTap: function() {
-    wx.navigateTo({
-      //url: '../admin/index'
-      url: '../searcher/index'
-      //url: '../search/index'
-    })
-  },
+  // bindhunterViewTap: function(event) {
+  //   console.log(event.target)
+  //   console.log(event.target.dataset.redirectUrl)
+  //   wx.navigateTo({
+  //     url: '../hunter/index'
+  //   })
+  // },
+  // bindconsignerViewTap: function() {
+  //   wx.navigateTo({
+  //     url: '../consigner/index'
+  //   })
+  // },
+  // bindadminViewTap: function() {
+  //   wx.navigateTo({
+  //     url: '../admin/index'
+  //   })
+  // },
+  // bindtaskboardViewTap: function() {
+  //   wx.navigateTo({
+  //      url: '../logs/logs'
+  //   })
+  // },
+  // bindmoreViewTap: function() {
+  //   wx.navigateTo({
+  //   })
+  // },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
